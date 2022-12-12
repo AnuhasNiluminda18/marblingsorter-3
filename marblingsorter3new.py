dea354f28352bd0578e526b389aae5949f676ec1
AnuhasNiluminda18 <119007959+AnuhasNiluminda18@users.noreply.github.com>
2022-12-12 19:21:17 +0530

    Update marblingsorter3new.py

--- marblingsorter3new.py
+++ marblingsorter3new.py
@@ -2,34 +2,7 @@ import streamlit as st
 import tensorflow as tf
 import streamlit as st
 import pickle
-from streamlit_option_menu import option_menu
-from  PIL import Image
 
-with st.sidebar:
-    choose = option_menu("App Gallery", ["About"],
-                         icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
-                         menu_icon="app-indicator", default_index=0,
-                         styles={
-        "container": {"padding": "5!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "#02ab21"},
-    }
-    )
-logo =Image.open('beefgradingcomparison.png')
-profile=Image.open('beefgradingcomparison.png')
-if choose == "About":
-    col1, col2 = st.columns( [0.8, 0.2])
-    with col1:               # To display the header text using css style
-        st.markdown(""" <style> .font {
-        font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
-        </style> """, unsafe_allow_html=True)
-        st.markdown('<p class="font">About the Creator</p>', unsafe_allow_html=True)    
-    with col2:               # To display brand log
-        st.image(logo, width=130 )
-    
-    st.write("Sharone Li is a data science practitioner, enthusiast, and blogger. She writes data science articles and tutorials about Python, data visualization, Streamlit, etc. She is also an amateur violinist who loves classical music.\n\nTo read Sharone's data science posts, please visit her Medium blog at: https://medium.com/@insightsbees")    
-    st.image(profile, width=700 )
 @st.cache(allow_output_mutation=True)
 def load_model():
    picklefile = open("emp-model.pkl", "rb")
@@ -45,7 +18,6 @@ st.write("""
          )
 from PIL import Image
 image = Image.open('beefgradingcomparison.png')
-
 
 st.image(image, caption='Made for your convenience')
 file = st.file_uploader("You can see the beef marbling status of your beef steak by uploading here", type=["jpg", "png"])
