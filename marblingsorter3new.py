ecf7e4b75498570631c4b623afbafebc108a7b2f
AnuhasNiluminda18 <119007959+AnuhasNiluminda18@users.noreply.github.com>
2022-12-12 06:32:00 +0530

    Update marblingsorter3new.py

--- marblingsorter3new.py
+++ marblingsorter3new.py
@@ -18,6 +18,15 @@ st.write("""
          )
 from PIL import Image
 image = Image.open('beefgradingcomparison.png')
+with st.sidebar:
+    choose = option_menu("App Gallery", ["About"],
+                         icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
 logo = Image.open('beefgradingcomparison.png')
 profile = Image.open('beefgradingcomparison.png')
 if choose == "About":
