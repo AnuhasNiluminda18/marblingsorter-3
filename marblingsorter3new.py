c5c140a7b58e6d5a7bfcd718af2f3c5daf0931bf
AnuhasNiluminda18 <119007959+AnuhasNiluminda18@users.noreply.github.com>
2022-12-13 23:57:47 +0530

    Update marblingsorter3new.py

--- marblingsorter3new.py
+++ marblingsorter3new.py
@@ -90,7 +90,7 @@ if choose == "Beef Marbling Sorter":
         print(
         #"This image most likely belongs to {} with a {:.2f} percent confidence.".format(class_names[np.argmax(score)], 100 * np.max(score))
 )
-if choose == "Beef price analysis"
+if choose == "Beef price analysis":
     url = 'https://www.masterclass.com/articles/what-is-marbling-in-meat-learn-about-the-different-types-of-marbling-and-what-factors-impact-marbling'
 
     if st.button('Open browser'):
