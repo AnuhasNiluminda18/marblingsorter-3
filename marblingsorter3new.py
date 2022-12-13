a8709d67205372a9c57ce05f8f3eb7bf4aa70792
AnuhasNiluminda18 <119007959+AnuhasNiluminda18@users.noreply.github.com>
2022-12-13 06:35:58 +0530

    Update marblingsorter3new.py

--- marblingsorter3new.py
+++ marblingsorter3new.py
@@ -36,4 +36,32 @@ st.markdown('<p class="font">Beef Marbling classifier</p>', unsafe_allow_html=Tr
 
 
             
+elif choose == "Photo Editing":
+    col1, col2 = st.columns( [0.8, 0.2])
+    with col1:               # To display the header text using css style
+        st.markdown(""" <style> .font {
+        font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
+        </style> """, unsafe_allow_html=True)
+        st.markdown('<p class="font">Upload your photo here...</p>', unsafe_allow_html=True)
+        
+    with col2:               # To display brand logo
+        st.image(logo,  width=150)
+    #Add file uploader to allow users to upload photos
+    uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        
+        col1, col2 = st.columns( [0.5, 0.5])
+        with col1:
+            st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
+            st.image(image,width=300)  
 
+        with col2:
+            st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
+
+            converted_img = np.array(image.convert('RGB')) 
+            gray_scale = cv2.cvtColor(converted_img, cv2.COLOR_RGB2GRAY)
+            inv_gray = 255 - gray_scale
+            blur_image = cv2.GaussianBlur(inv_gray, (125,125), 0, 0)
+            sketch = cv2.divide(gray_scale, 255 - blur_image, scale=256)
+            st.image(sketch, width=300)
