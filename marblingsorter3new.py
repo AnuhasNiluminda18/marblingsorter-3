5d41211ba2677ab1733ececf52ecbcb4a57d985d
AnuhasNiluminda18 <119007959+AnuhasNiluminda18@users.noreply.github.com>
2022-12-13 05:59:48 +0530

    Update marblingsorter3new.py

--- marblingsorter3new.py
+++ marblingsorter3new.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import streamlit as st
 import pickle
 from streamlit_option_menu import option_menu
+from  PIL import Image
 
 with st.sidebar:
     choose = option_menu("App Gallery", ["About", "Photo Editing", "Project Planning", "Python e-Course", "Contact"],
@@ -15,7 +16,19 @@ with st.sidebar:
         "nav-link-selected": {"background-color": "#02ab21"},
     }
     )
-
+if choose == "About":
+    col1, col2 = st.columns( [0.8, 0.2])
+    with col1:               # To display the header text using css style
+        st.markdown(""" <style> .font {
+        font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
+        </style> """, unsafe_allow_html=True)
+        st.markdown('<p class="font">About the Creator</p>', unsafe_allow_html=True)    
+    with col2:               # To display brand log
+       # st.image(logo, width=130 )
+    
+    st.write("Sharone Li is a data science practitioner, enthusiast, and blogger. She writes data science articles and tutorials about Python, data visualization, Streamlit, etc. She is also an amateur violinist who loves classical music.\n\nTo read Sharone's data science posts, please visit her Medium blog at: https://medium.com/@insightsbees")    
+    #st.image(profile, width=700 )
+    
 st.markdown(""" <style> .font {
 font-size:50px ; font-family: 'Cooper Black'; color: #FF9633;} 
 </style> """, unsafe_allow_html=True)
