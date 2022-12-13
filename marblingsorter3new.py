7e23e7e40048cfeeb08463c8edc4533a999494aa
AnuhasNiluminda18 <119007959+AnuhasNiluminda18@users.noreply.github.com>
2022-12-13 06:47:53 +0530

    Update marblingsorter3new.py

--- marblingsorter3new.py
+++ marblingsorter3new.py
@@ -37,31 +37,54 @@ st.markdown('<p class="font">Beef Marbling classifier</p>', unsafe_allow_html=Tr
 
             
 if choose == "Photo Editing":
-    col1, col2 = st.columns( [0.8, 0.2])
-    with col1:               # To display the header text using css style
-        st.markdown(""" <style> .font {
-        font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
-        </style> """, unsafe_allow_html=True)
-        st.markdown('<p class="font">Upload your photo here...</p>', unsafe_allow_html=True)
-        
-    with col2:               # To display brand logo
-        #st.image(logo,  width=150)
-    #Add file uploader to allow users to upload photos
-        uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        
-        col1, col2 = st.columns( [0.5, 0.5])
-        with col1:
-            st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
-            st.image(image,width=300)  
+    @st.cache(allow_output_mutation=True)
+def load_model():
+   picklefile = open("mymodelTransfer.pkl", "rb")
+   model = pickle.load(picklefile)
+   return model
 
-        with col2:
-            st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
+with st.spinner('Model is being loaded..'):
+  model=load_model()
+from PIL import Image, ImageOps
+st.write("""
+         # Beef Marbling classifier
+         """
+         )
+from PIL import Image
+image = Image.open('beefgradingcomparison.png')
 
-            converted_img = np.array(image.convert('RGB')) 
-            gray_scale = cv2.cvtColor(converted_img, cv2.COLOR_RGB2GRAY)
-            inv_gray = 255 - gray_scale
-            blur_image = cv2.GaussianBlur(inv_gray, (125,125), 0, 0)
-            sketch = cv2.divide(gray_scale, 255 - blur_image, scale=256)
-            st.image(sketch, width=300)
+st.image(image, caption='Made for your convenience')
+file = st.file_uploader("You can see the beef marbling status of your beef steak by uploading here", type=["jpg", "png"])
+class_names=['Group1-Select','Group2-Select','Group3-Choice','Group4-Choice','Group5-Prime','Group6-Prime']
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+st.set_option('deprecation.showfileUploaderEncoding', False)
+def import_and_predict(image_data, model):
+    
+        size = (224, 224)    
+        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+        image = np.asarray(image)
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        #img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
+        
+        img_reshape = img[np.newaxis,...]
+    
+        predictions = model.predict(img_reshape)
+        
+        return predictions
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    predictions = import_and_predict(image, model)
+    score = tf.nn.softmax(predictions[0])
+    #st.write(predictions)
+    #st.write(score)
+    pred_class=class_names[np.argmax(predictions)]
+    st.write("Predicted Class:",pred_class)
+    st.write("Place your feedback here [link](https://docs.google.com/forms/d/e/1FAIpQLSez6MK1CuUisH-j1rBjx1Bpoe1JwgA1bAIlV5MMD1rmbkJ1Bg/viewform?usp=sf_link)")
+    print(
+    #"This image most likely belongs to {} with a {:.2f} percent confidence.".format(class_names[np.argmax(score)], 100 * np.max(score))
+)
