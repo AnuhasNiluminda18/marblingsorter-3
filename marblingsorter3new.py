a9f8edf3bc49fb065b4dc44619597fe0e12a5778
AnuhasNiluminda18 <119007959+AnuhasNiluminda18@users.noreply.github.com>
2022-12-13 05:56:22 +0530

    Update marblingsorter3new.py

--- marblingsorter3new.py
+++ marblingsorter3new.py
@@ -3,6 +3,18 @@ import tensorflow as tf
 import streamlit as st
 import pickle
 
+with st.sidebar:
+    choose = option_menu("App Gallery", ["About", "Photo Editing", "Project Planning", "Python e-Course", "Contact"],
+                         icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    }
+    )
+
 st.markdown(""" <style> .font {
 font-size:50px ; font-family: 'Cooper Black'; color: #FF9633;} 
 </style> """, unsafe_allow_html=True)
