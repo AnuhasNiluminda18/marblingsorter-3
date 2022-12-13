6cf0139c0d952502314aa3c677b747ad7e0d3934
AnuhasNiluminda18 <119007959+AnuhasNiluminda18@users.noreply.github.com>
2022-12-13 06:06:36 +0530

    Update marblingsorter3new.py

--- marblingsorter3new.py
+++ marblingsorter3new.py
@@ -34,6 +34,26 @@ font-size:50px ; font-family: 'Cooper Black'; color: #FF9633;}
 </style> """, unsafe_allow_html=True)
 st.markdown('<p class="font">Beef Marbling classifier</p>', unsafe_allow_html=True)
 
+elif choose == "Photo Editing":
+    col1, col2 = st.columns( [0.8, 0.2])
+    with col1:               # To display the header text using css style
+        st.markdown(""" <style> .font {
+        font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
+        </style> """, unsafe_allow_html=True)
+        st.markdown('<p class="font">Upload your photo here...</p>', unsafe_allow_html=True)
+        
+    with col2:               # To display brand logo
+        st.image(logo,  width=150)
+    #Add file uploader to allow users to upload photos
+    uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        
+        col1, col2 = st.columns( [0.5, 0.5])
+        with col1:
+            st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
+            st.image(image,width=300)  
+            
 @st.cache(allow_output_mutation=True)
 def load_model():
    picklefile = open("emp-model.pkl", "rb")
