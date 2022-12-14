0e834e739892f0bf17163f27c4fec809c74ba172
AnuhasNiluminda18 <119007959+AnuhasNiluminda18@users.noreply.github.com>
2022-12-14 21:59:00 +0530

    Update marblingsorter3new.py

--- marblingsorter3new.py
+++ marblingsorter3new.py
@@ -19,10 +19,10 @@ with st.sidebar:
     }
     )
 if choose == "About":
-    col1, col2 = st.columns( [0.5, 0.5])
+    col1, col2 = st.columns( [0.6, 0.4])
     with col1:               # To display the header text using css style
         st.markdown(""" <style> .font {
-        font-size:2px ; font-family: 'Cooper Black'; color: #070701;} 
+        font-size:2px ; font-family: 'Cooper Black'; color: #FF9633;} 
         </style> """, unsafe_allow_html=True)
         st.markdown('<p class="font">Marbling is the visible unsaturated (healthy) intramuscular fat that accumulates within the muscle and between the muscle fibre bundles. Visually, marbling is soft intramuscular (between the muscle fibre) fat made up of polyunsaturated, monounsaturated and saturated fats</p>', unsafe_allow_html=True)    
     with col2:               # To display brand log
