f9e2de65fb9fdf208614d537494899b244d67346
AnuhasNiluminda18 <119007959+AnuhasNiluminda18@users.noreply.github.com>
2022-12-16 22:56:09 +0530

    Update marblingsorter3new.py

--- marblingsorter3new.py
+++ marblingsorter3new.py
@@ -90,7 +90,7 @@ if choose == "Beef Marbling Sorter":
         #"This image most likely belongs to {} with a {:.2f} percent confidence.".format(class_names[np.argmax(score)], 100 * np.max(score))
 )
 if choose == "Beef price analysis":
-    DEMO_IMAGE = 'pavan-kunchala-QR.png'
+    DEMO_IMAGE = 'beefgradingcomparison.png'
 
 #title of the web-app
     st.title('QR Code Decoding with OpenCV')
