1c4666889a27accb8b63777292cb0dded3311570
AnuhasNiluminda18 <119007959+AnuhasNiluminda18@users.noreply.github.com>
2022-12-17 00:18:44 +0530

    Update marblingsorter3new.py

--- marblingsorter3new.py
+++ marblingsorter3new.py
@@ -96,49 +96,7 @@ if choose == "Beef price analysis":
 
 
 
-    DEMO_IMAGE = 'beefgradingcomparison.png'
-
-#title of the web-app
-    st.title('QR Code Decoding with OpenCV')
-
-    @st.cache
-    def show_qr_detection(img,pts):
     
-        pts = np.int32(pts).reshape(-1, 2)
-    
-        for j in range(pts.shape[0]):
-        
-            cv2.line(img, tuple(pts[j]), tuple(pts[(j + 1) % pts.shape[0]]), (255, 0, 0), 5)
-        
-        for j in range(pts.shape[0]):
-            cv2.circle(img, tuple(pts[j]), 10, (255, 0, 255), -1)
-
-
-    @st.cache
-    def qr_code_dec(image):
-    
-        decoder = cv2.QRCodeDetector()
-    
-        data, vertices, rectified_qr_code = decoder.detectAndDecode(image)
-          
-        
-        if len(data) > 0:
-            print("Decoded Data: '{}'".format(data))
-
-    # Show the detection in the image:
-            show_qr_detection(image, vertices)
-        
-            rectified_image = np.uint8(rectified_qr_code)
-        
-            rectified_image = cv2.putText(rectified_image,decoded_data,(50,350),fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale = 2,
-                color = (250,225,100),thickness =  3, lineType=cv2.LINE_AA)
-            
-            decoded_data = 'Decoded data: '+ data
-        
-           
-      
-        return decoded_data
-
 
     
     
@@ -163,7 +121,7 @@ if choose == "Beef price analysis":
     image, caption=f"Original Image", use_column_width=True
     ) 
 
-
+    
 
     st.subheader('Decoded data')
 
