d5e4221a7bb959760c09cbdb29b9b58b03706828
AnuhasNiluminda18 <119007959+AnuhasNiluminda18@users.noreply.github.com>
2022-12-16 22:33:34 +0530

    Update marblingsorter3new.py

--- marblingsorter3new.py
+++ marblingsorter3new.py
@@ -116,22 +116,22 @@ if choose == "Beef price analysis":
     @st.cache
     def qr_code_dec(image):
     
-    decoder = cv2.QRCodeDetector()
+        decoder = cv2.QRCodeDetector()
     
-    data, vertices, rectified_qr_code = decoder.detectAndDecode(image)
+        data, vertices, rectified_qr_code = decoder.detectAndDecode(image)
     
-    if len(data) > 0:
-        print("Decoded Data: '{}'".format(data))
+        if len(data) > 0:
+            print("Decoded Data: '{}'".format(data))
 
     # Show the detection in the image:
-        show_qr_detection(image, vertices)
+            show_qr_detection(image, vertices)
         
-        rectified_image = np.uint8(rectified_qr_code)
+            rectified_image = np.uint8(rectified_qr_code)
         
-        decoded_data = 'Decoded data: '+ data
+            decoded_data = 'Decoded data: '+ data
         
-        rectified_image = cv2.putText(rectified_image,decoded_data,(50,350),fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale = 2,
-            color = (250,225,100),thickness =  3, lineType=cv2.LINE_AA)
+            rectified_image = cv2.putText(rectified_image,decoded_data,(50,350),fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale = 2,
+                color = (250,225,100),thickness =  3, lineType=cv2.LINE_AA)
         
         
         st.subheader('Orginal Image')
